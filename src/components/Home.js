1d36501cd2b48a695857444b16f6f199cfacb536
Rafael <rafael.mendes1101@gmail.com>
2021-11-23 14:06:48 +0000

    added user context and set user logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,40 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/user";
+import { getUsers } from "../utils/api";
+import { Link } from "react-router-dom";
 export default function Home() {
-  return <h1>Welcome to the NC news App</h1>;
+  //   console.log("here <===");
+  const [users, setUsers] = useState([]);
+  const { setCurrentUser } = useContext(UserContext);
+  useEffect(() => {
+    getUsers().then((users) => {
+      setUsers(users);
+    });
+  }, []);
+
+  return (
+    <section>
+      <h1>Welcome to the NC news App</h1>
+      <h2>Who are you?</h2>
+      <div className="user_selection">
+        {users.map((user) => {
+          return (
+            <Link
+              key={user.username}
+              to="/articles"
+              onClick={() => {
+                setCurrentUser(user);
+              }}
+            >
+              <img
+                src={user.avatar_url}
+                alt={user.name}
+                className="user_avatar"
+              ></img>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
