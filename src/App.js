d56ccbe867f8782e325aa758af0e7c12495ff082
Rafael <rafael.mendes1101@gmail.com>
2021-11-23 03:06:09 +0000

    Added user context

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Articles from "./components/Articles";
+import Home from "./components/Home";
+import { UserContext } from "./contexts/user";
+import { useState } from "react";
+
 function App() {
+  const [users, setUsers] = useState({
+    username: "",
+    avatar_url: "",
+    name: "",
+  });
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
         <Nav />
         <Routes>
-          <Route to="/" element={<Articles />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </div>
     </BrowserRouter>
