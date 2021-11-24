4fa3e80b6f18495d2a24c40df687011df7a3f09d
Rafael <rafael.mendes1101@gmail.com>
2021-11-24 12:32:06 +0000

    added Articles logic

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import { getArticles } from "../utils/api";
+import Article from "./Article";
+
 export default function Articles() {
+  const [articles, setArticles] = useState({});
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    let query = { sort: "article_id", order: "ASC" };
+    getArticles(query)
+      .then((articles) => {
+        setArticles(articles);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  }, []);
+  if (isLoading) return <p>Loading...</p>;
   return (
     <main className="Articles">
       <h1>Articles</h1>
+      <ul>
+        {articles.map((article) => {
+          return (
+            <li key={article.article_id}>
+              <Article
+                title={article.title}
+                author={article.author}
+                body={article.body}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
