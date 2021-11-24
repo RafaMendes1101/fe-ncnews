5cafa8fb401ffabb6b20c78b103401d46fe21f9a
Rafael <rafael.mendes1101@gmail.com>
2021-11-24 14:07:41 +0000

    added Links to Topics and Articles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,8 @@ export default function Nav() {
   return (
     <nav className="Nav">
       <Link to="/home">Home</Link>
+      <Link to="/topics">Topics</Link>
+      <Link to="/articles">Articles</Link>
     </nav>
   );
 }
