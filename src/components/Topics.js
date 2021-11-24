9ff25ef518fa4bd04e70e473a22faface2c84b84
Rafael <rafael.mendes1101@gmail.com>
2021-11-24 14:11:59 +0000

    initial Topics setup

---
+++ src/components/Topics.js
@@ -0,0 +1,11 @@
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../contexts/user";
+import { Navigate } from "react-router-dom";
+
+export default function Topics() {
+  return (
+    <main className="Topics">
+      <h1>Topics</h1>
+    </main>
+  );
+}
