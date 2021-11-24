e14b10e165f7fecb5b91e5764129e5a3a6c77845
Rafael <rafael.mendes1101@gmail.com>
2021-11-24 15:46:56 +0000

    Listing topics

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,11 +1,38 @@
 import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../contexts/user";
 import { Navigate } from "react-router-dom";
-
+import { getTopics } from "../utils/api";
 export default function Topics() {
+  const [topics, setTopics] = useState({});
+  const [isLoading, setLoading] = useState(true);
+  const { currentUser } = useContext(UserContext);
+
+  useEffect(() => {
+    setLoading(true);
+    getTopics()
+      .then((topics) => {
+        setTopics(topics);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  }, []);
+  if (isLoading) return <p>Loading...</p>;
+  if (currentUser.username === "") {
+    alert("set a user");
+    return <Navigate to="/home" />;
+  }
+
   return (
     <main className="Topics">
       <h1>Topics</h1>
+      <ul>
+        {topics.map((topic) => {
+          return <li key={topic.slug}>{topic.slug}</li>;
+        })}
+      </ul>
     </main>
   );
 }
