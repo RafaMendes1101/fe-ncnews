f1c26fd592f5382b3d5cda9f7917d500242ed69c
Rafael <rafael.mendes1101@gmail.com>
2021-11-25 10:55:06 +0000

    Added ArticleById logic

--- src/components/ArticleById.js
+++ src/components/ArticleById.js
@@ -1,3 +1,22 @@
+import { useEffect, useState, useContext } from "react";
+import { AppContext } from "../contexts/contexts";
+import { getArticleById } from "../utils/api";
+import { Navigate } from "react-router-dom";
 export default function ArticleById() {
-  return <h1>Article By Id</h1>;
+  const { articleId, currentUser } = useContext(AppContext);
+  const [article, setArticle] = useState({});
+  useEffect(() => {
+    getArticleById(articleId).then((article) => {
+      setArticle(article);
+    });
+  }, [articleId]);
+  if (currentUser.username === "") {
+    alert("set a user");
+    return <Navigate to="/" />;
+  }
+  return (
+    <>
+      <h2>{article.title}</h2>
+    </>
+  );
 }
