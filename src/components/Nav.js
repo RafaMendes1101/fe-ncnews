d2aa20c7563f9e1a98e145ad8af47f2372387c7b
Rafael <rafael.mendes1101@gmail.com>
2021-11-25 10:19:49 +0000

    Added | separators

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav className="Nav">
-      <Link to="/">Home</Link>
+      <Link to="/">Home</Link> {" | "}
       <Link to="/topics">Topics</Link>
+      {" | "}
       <Link to="/articles">Articles</Link>
     </nav>
   );
