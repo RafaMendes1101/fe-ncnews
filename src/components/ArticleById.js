224b5aef174d2714acfdd6ef2a63bee1b74aa3f3
Rafael <rafael.mendes1101@gmail.com>
2021-11-25 17:42:55 +0000

    added ExpandComments logic

--- src/components/ArticleById.js
+++ src/components/ArticleById.js
@@ -1,11 +1,15 @@
 import { useEffect, useState, useContext } from "react";
 import { AppContext } from "../contexts/contexts";
-import { getArticleById, getAuthorName } from "../utils/api";
+import { getArticleById, getAuthorName, getComments } from "../utils/api";
 import { Navigate } from "react-router-dom";
+import Votes from "./Votes";
 export default function ArticleById() {
   const { articleId, currentUser } = useContext(AppContext);
   const [article, setArticle] = useState({});
   const [authorName, setAuthorName] = useState("");
+  const [comments, setComments] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     getArticleById(articleId).then((article) => {
       setArticle(article);
@@ -14,6 +18,20 @@ export default function ArticleById() {
       });
     });
   }, [articleId]);
+  const ExpandComments = (props) => {
+    const toggleIsOpen = () => {
+      getComments(article.article_id).then((comments) => {
+        setComments(comments);
+      });
+      setIsOpen((prevIsOpen) => !prevIsOpen);
+    };
+    return (
+      <div className="Comments">
+        <span onClick={toggleIsOpen}>See comments</span>
+        {isOpen && props.children}
+      </div>
+    );
+  };
 
   if (currentUser.username === "") {
     alert("set a user");
@@ -26,6 +44,25 @@ export default function ArticleById() {
         Written by: <strong>{authorName}</strong>
       </p>
       <p>{article.body}</p>
+      <ExpandComments>
+        <h1>Comments</h1>
+        <ul>
+          {comments.length > 0 ? (
+            comments.map((comment) => {
+              return (
+                <li key={comment.comment_id}>
+                  <p>{comment.body}</p>
+                  <span>
+                    <Votes comment={comment} />
+                  </span>
+                </li>
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </ul>
+      </ExpandComments>
     </main>
   );
 }
