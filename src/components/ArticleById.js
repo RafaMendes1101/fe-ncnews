ccc76c517522f5871259556e103803c184cfa81a
Rafael <rafael.mendes1101@gmail.com>
2021-11-25 12:04:49 +0000

    Added authorName, setAuthorname state

--- src/components/ArticleById.js
+++ src/components/ArticleById.js
@@ -1,22 +1,31 @@
 import { useEffect, useState, useContext } from "react";
 import { AppContext } from "../contexts/contexts";
-import { getArticleById } from "../utils/api";
+import { getArticleById, getAuthorName } from "../utils/api";
 import { Navigate } from "react-router-dom";
 export default function ArticleById() {
   const { articleId, currentUser } = useContext(AppContext);
   const [article, setArticle] = useState({});
+  const [authorName, setAuthorName] = useState("");
   useEffect(() => {
     getArticleById(articleId).then((article) => {
       setArticle(article);
+      getAuthorName(article.author).then((authorName) => {
+        setAuthorName(authorName);
+      });
     });
   }, [articleId]);
+
   if (currentUser.username === "") {
     alert("set a user");
     return <Navigate to="/" />;
   }
   return (
-    <>
+    <main className="ArticleById">
       <h2>{article.title}</h2>
-    </>
+      <p>
+        Written by: <strong>{authorName}</strong>
+      </p>
+      <p>{article.body}</p>
+    </main>
   );
 }
