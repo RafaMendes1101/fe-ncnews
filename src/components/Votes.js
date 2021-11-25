910ce58c586bc6f1a0a95ad589567f5c1e0721b7
Rafael <rafael.mendes1101@gmail.com>
2021-11-25 17:42:07 +0000

    added Votes component

---
+++ src/components/Votes.js
@@ -0,0 +1,23 @@
+import { updateVotes } from "../utils/api";
+import { useState } from "react";
+export default function Votes({ comment }) {
+  const [vote, setVote] = useState(0);
+  const handleVote = (e, incVote) => {
+    setVote((prevVote) => {
+      return prevVote + incVote;
+    });
+    updateVotes(e.target.name, incVote);
+  };
+  return (
+    <div>
+      Votes: {comment.votes + vote}{" "}
+      <button
+        onClick={(e) => handleVote(e, 1)}
+        value={comment.votes}
+        name={comment.comment_id}
+      >
+        +
+      </button>
+    </div>
+  );
+}
