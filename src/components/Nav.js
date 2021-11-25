c1e3cae2ad18e0b223e0e9bde4997f8775c3242c
Rafael <rafael.mendes1101@gmail.com>
2021-11-25 07:03:07 +0000

    Change Home lint to=/

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav className="Nav">
-      <Link to="/home">Home</Link>
+      <Link to="/">Home</Link>
       <Link to="/topics">Topics</Link>
       <Link to="/articles">Articles</Link>
     </nav>
